61b84132440bdc1a4ba89aa994dffc4161d723ae
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 08:49:48 +0200

    backend: add theater server start

---
+++ backend/theater/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import config from "config";
+import logger from "./utils/logger";
+import database from './database/default';
+import expressApp from './express-app';
+
+declare global {
+    namespace Express {
+        interface Request {
+            jwt: string;
+            user: {
+                _id: string;
+                email: string;
+            };
+        }
+    }
+}
+
+const StartServer = async () => {
+
+    const app = express();
+
+    database.databaseConnection();
+
+    expressApp(app);
+
+    app.listen(config.get<number>("PORT"), () => {
+        logger.info(`Listening to port ${config.get<number>("PORT")}`);
+    })
+        .on('error', (err) => {
+            logger.error(err);
+            process.exit();
+        })
+
+}
+
+StartServer();
