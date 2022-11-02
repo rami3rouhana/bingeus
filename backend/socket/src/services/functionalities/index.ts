9ef956737944bcfcd747d6c4bdf654a24b7326f0
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 11:45:26 +0200

    backend: add socket functionality

--- backend/socket/src/services/functionalities/index.ts
+++ backend/socket/src/services/functionalities/index.ts
@@ -4,23 +4,25 @@ import { Channel } from "amqplib";
 import logger from "../../utils/logger";
 import { PublishMessage, userCredentials, SubscribeMessage } from '../../utils';
 import validateSignature from '../middleware/validateUser';
+import axios from 'axios';
 import config from 'config';
 import services from './service.class';
 
 
 declare module 'socket.io' {
     interface Socket {
-        _id: string
-        username: string
+        _id: string;
+        admin: boolean;
+        theater: string;
     }
 }
 
 
-export default async (app: Express, io: Server<any, any, any, any>, channel: Channel, callback) => {
+export default async (app: Express, io: Server<any, any, any, any>, channel: Channel) => {
 
     const service = new services();
 
-    SubscribeMessage(channel, service, callback)
+    SubscribeMessage(channel, service)
 
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
@@ -28,23 +30,34 @@ export default async (app: Express, io: Server<any, any, any, any>, channel: Cha
     const theaterIo = io.of('/theater');
 
     theaterIo.use(async (socket, next) => {
-        if (socket.handshake.auth.token) {
-            let payloads = await validateSignature(socket.handshake.auth.token) as unknown as userCredentials;
-            if (payloads instanceof Error) {
-                return logger.error(payloads);
-            }
+
+        let payloads = await validateSignature(socket.handshake.auth.token) as unknown as userCredentials;
+
+        if (payloads)
             socket._id = payloads._id;
-            payloads = { ...payloads, _id: socket.handshake.auth.room }
-            const pay = {
-                payloads,
-                event: "GET_THEATER"
-            }
-            PublishMessage(channel, config.get<string>('THEATER_SERVICE'), JSON.stringify(pay));
-            next();
-        } else {
-            return new Error("Token Invalid");
+
+        let theater
+        try {
+            theater = await axios.get(`http://localhost:8002/theater/${socket.handshake.auth.theater}`);
+        } catch (error) {
+            return logger.error("Theater Doesn't Exist");
         }
+
+        if (theater.data.adminId === socket._id) {
+            socket.admin = true;
+        }
+
+        if (theater.data.blockedList.includes(socket._id)) {
+            throw new Error("User is blocked");
+        }
+
+        socket.theater = socket.handshake.auth.theater;
+
+        socket.join(socket.handshake.auth.theater);
+
+        next();
     })
+    
     theaterIo.on('connection', (socket) => {
 
         logger.info(`admin ${socket._id} connected`);
@@ -57,13 +70,11 @@ export default async (app: Express, io: Server<any, any, any, any>, channel: Cha
                 socket.broadcast.emit('receive message', msg);
             }
 
-            socket.to(room).emit('receive message', msg);
+            if (socket._id)
+                socket.to(room).emit('receive message', msg);
         });
 
-        // socket.on('join room', (room,cb) =>{
-        //     socket.join(room);
-        //     cb(`User ${socket.id} has Joined ${room}`)
-        // })
+
 
         socket.on('join room', (room: string) => {
             socket.join(room);
