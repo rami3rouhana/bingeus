52f8e77e9ea52baed7005240c2355dea7f77bd15
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 08:49:16 +0200

    backend: add theater config

---
+++ backend/theater/config/default.ts
@@ -0,0 +1,20 @@
+import dotEnv from "dotenv";
+
+if (process.env.npm_lifecycle_event !== "prod") {
+    const configFile = `./${process.env.npm_lifecycle_event}.env`;
+    dotEnv.config({ path: configFile });
+} else {
+    dotEnv.config();
+}
+
+export default {
+    PORT: process.env.PORT as unknown as number,
+    DB_URL: process.env.MONGODB_URI,
+    APP_SECRET: process.env.APP_SECRET,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+    SOCKET_SERVICE: "SOCKET_SERVICE",
+    THEATER_SERVICE: "THEATER_SERVICE",
+    POLL_SERVICE: "POLL_SERVICE",
+    QUEUE_NAME:"THEATER_QUEUE"
+};
