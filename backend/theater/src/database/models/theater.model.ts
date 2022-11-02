038d66af04afd39a54156a6a50040496ea1b72ac
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 08:52:18 +0200

    backend: add theater model

---
+++ backend/theater/src/database/models/theater.model.ts
@@ -0,0 +1,89 @@
+import mongoose, { ObjectId } from "mongoose";
+
+export interface UserInput {
+    adminId: string,
+    name: string,
+    showing: {
+        _id?: string,
+        title: string,
+        poster: string,
+        description: string,
+    },
+    polls: [{
+        pollId?: string,
+        title: string,
+        options: [{
+            optionId: string,
+            name: string,
+            votes: number
+        }]
+    }],
+    playlist: [{
+        _id?: string,
+        name: string,
+        video: string,
+        current?:boolean,
+        description: string,
+    }],
+    blockedList: [{
+        userId: string,
+        name: string,
+        image: string
+    }]
+}
+
+
+export interface TheaterDocument extends UserInput, mongoose.Document {
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface userBlock {
+    _id: ObjectId;
+    blockedList: [{
+        userId: ObjectId,
+        name: string,
+        image: string
+    }]
+}
+
+export interface theaterVote {
+    _id?: string,
+    title: string,
+    options: [{
+        _id?: string,
+        name: string,
+        votes?: string[]
+    }],
+}
+
+const theaterSchema = new mongoose.Schema(
+    {
+        adminId: { type: String },
+        name: { type: String, required: true },
+        showing: {
+            title: { type: String, required: true },
+            poster: { type: String, required: true },
+            description: { type: String, required: true },
+        },
+        playlist: [{
+            name: { type: String },
+            video: { type: String },
+            current: { type: Boolean, default: false },
+            description: { type: String },
+        }],
+        blockedList: [{ type: String }]
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                delete ret.__v;
+            }
+        },
+        timestamps: true
+    }
+);
+
+const TheaterModel = mongoose.model<TheaterDocument>("theater", theaterSchema);
+
+export default TheaterModel;
