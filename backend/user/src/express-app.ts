005048f0ae10edcdcd627caab11712617de57cf5
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 17:30:56 +0200

    backend: add expressapp  user

--- backend/user/src/express-app.ts
+++ backend/user/src/express-app.ts
@@ -1,14 +1,18 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import routes from './api/routes';
+import { CreateChannel } from './utils';
 
-export default (app: Express) => {
+
+export default async (app: Express) => {
+
+    const channel = await CreateChannel();
 
     app.use(express.json());
     app.use(cors());
     app.use(express.static(__dirname + '/public'));
 
 
-    routes(app);
+    routes(app, channel);
 
 }
