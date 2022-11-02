7ad639375abd993533d043e04195989c9de256f3
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-02 08:45:35 +0200

    backend: add socket express app

---
+++ backend/socket/src/express-app.ts
@@ -0,0 +1,33 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import services from './api/services';
+import http from 'http';
+import { instrument } from "@socket.io/admin-ui";
+import { Server } from "socket.io";
+import { CreateChannel } from "./utils";
+
+
+export default async (app: Express, server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>) => {
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(express.static(__dirname + '/public'));
+
+    const service = await CreateChannel();
+
+    const io = new Server(server, {
+        cors: {
+            origin: ["https://admin.socket.io"],
+            credentials: true
+        },
+    });
+
+    instrument(io, {
+        namespaceName: "/",
+        auth: false
+    });
+
+    services(app, io, service.channel,service.callback);
+
+}
+
