60e962c7ccd5550e15701b3269e60bb5a0a3ac2f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-03 10:17:19 +0200

    frontend: add global types

---
+++ frontend/src/context/types.tsx
@@ -0,0 +1,19 @@
+import { Dispatch } from 'react';
+
+export interface GlobalStateInterface {
+  id: string,
+  name: string,
+  loggedIn: boolean,
+  role: number,
+  children?: React.ReactNode
+}
+
+export type ActionType = {
+  type: string;
+  payload?: any;
+};
+
+export type ContextType = {
+  globalState: GlobalStateInterface;
+  dispatch: Dispatch<ActionType>;
+};
