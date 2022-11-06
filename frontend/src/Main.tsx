72f3728cc34abc709edfab9e0d6be1d24a76a9d1
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-06 16:25:21 +0200

    frontend: add signup route

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login';
+import SignUpPage from './pages/Signup';
 
 const Main = () => {
 
   return (
     <Routes>
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
     </Routes>
   );
 }
