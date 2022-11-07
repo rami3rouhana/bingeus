5dcccb57a0283efdd56bf64fef88835c66030628
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-07 09:53:35 +0200

    frontend : add userInfo main functions

--- frontend/src/components/ui/UserInfo/index.tsx
+++ frontend/src/components/ui/UserInfo/index.tsx
@@ -1,12 +1,23 @@
 import { GlobalStateContext } from "../../../context/GlobalState";
-import {useContext} from 'react';
+import { useContext, useRef } from 'react';
 
 const UserInfo = () => {
     const userInfo = useContext(GlobalStateContext);
+    const ref = useRef<HTMLInputElement>(null);
+
+    const handleImageUpdate = () => {
+        ref.current?.click();
+    }
+
+    const handleProfilePopUp = () => {
+
+    }
 
     return (
         <>
-
+            <img src={userInfo.user.image} onClick={handleImageUpdate} />
+            <input ref={ref} type='file'></input>
+            <button onClick={handleProfilePopUp} >Edit Profile</button>
         </>
     )
 }
