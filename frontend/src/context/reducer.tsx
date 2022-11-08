7af6fa1de0d27327dcb5f6b8d0b9737adbda7ceb
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-08 10:45:32 +0200

    frontend: add unblock state

--- frontend/src/context/reducer.tsx
+++ frontend/src/context/reducer.tsx
@@ -2,11 +2,12 @@ import { ActionType, GlobalStateInterface } from './types';
 import { initialState } from './index';
 
 const Reducer = (state: GlobalStateInterface, action: ActionType): any => {
+  const user = state.user;
   switch (action.type) {
     case 'LOGIN':
       return {
         ...state,
-        user: action.payload,
+        user: action.payload
       };
     case 'REGISTER':
       return {
@@ -14,13 +15,21 @@ const Reducer = (state: GlobalStateInterface, action: ActionType): any => {
         user: action.payload,
       };
     case 'EDIT_PROFILE':
-      const user = state.user
-      for(let updated in action.payload){
-        user[updated] = action.payload[updated]
+
+      for (let updated in action.payload) {
+        user[updated] = action.payload[updated];
       }
       return {
         ...state,
         user: user,
+      };
+    case 'UNBLOCK':
+      const userBlockedList = state.user.blockedList;
+      const blockedList = userBlockedList.filter((e: any) => e.userId !== action.payload);
+      user['blockedList'] = blockedList;
+      return {
+        ...state,
+        blockedList: blockedList,
       };
     case 'ERROR':
       return {
