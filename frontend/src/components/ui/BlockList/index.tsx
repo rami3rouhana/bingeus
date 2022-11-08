8250cf2e3d0fe5f5c3224a693c7c09c9d0190ed3
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-08 10:46:51 +0200

    frontend: add unblock display

--- frontend/src/components/ui/BlockList/index.tsx
+++ frontend/src/components/ui/BlockList/index.tsx
@@ -1,12 +1,20 @@
 import { GlobalStateContext } from "../../../context/GlobalState";
-import {useContext} from 'react';
+import { useContext } from 'react';
 
 const BlockList = () => {
     const userInfo = useContext(GlobalStateContext);
-
     return (
         <>
-
+            <ul>
+                {userInfo.user.blockedList.map((user: any) => {
+                    return (
+                        <li key={user._id}>
+                            <img src={user.image} />
+                            <span>{user.name}</span>
+                            <button onClick={async () => await userInfo.unblock(user.userId)}>Unblock</button>
+                        </li>)
+                })}
+            </ul>
         </>
     )
 }
