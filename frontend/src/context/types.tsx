ee49a1f223c22bbd31651e7123cc117b9b54f611
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-08 10:45:56 +0200

    frontend: refactor type

--- frontend/src/context/types.tsx
+++ frontend/src/context/types.tsx
@@ -5,6 +5,7 @@ export interface UserStateInterface {
   name?: string,
   image?: string,
   loggedIn: boolean,
+  blockedList:object[],
   email?: string,
   role: number
   children?: React.ReactNode
