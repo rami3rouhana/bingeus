6f096402bcf3638b0e265adbab1ec5332fe2a426
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-09 15:36:17 +0200

    frontend: add get user theaters

--- frontend/src/components/ui/UserTheaters/index.tsx
+++ frontend/src/components/ui/UserTheaters/index.tsx
@@ -1,11 +1,26 @@
 import { GlobalStateContext } from "../../../context/GlobalState";
-import {useContext} from 'react';
+import { useContext, useEffect, useState } from 'react';
+import TheaterAdd from "../Models/TheaterAdd";
 
 const UserTheaters = () => {
     const userInfo = useContext(GlobalStateContext);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const getTheaters = async () => {
+            await userInfo.getUserTheaters();
+        }
+        getTheaters();
+    }, [])
+
 
     return (
         <>
+            <button onClick={() => setShowModal(true)}>+</button>
+            {showModal ? <TheaterAdd setShowModal={setShowModal} /> : null}
+            {userInfo.user.theaters?.map((theater: any) => {
+                return <div key={theater._id}><img src={theater.showing.poster} />{theater.showing.title}<p>{theater.showing.description}</p></div>
+            })}
 
         </>
     )
