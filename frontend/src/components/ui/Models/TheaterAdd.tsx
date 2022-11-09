8162822b9f9f19ebf1f1fdc3e5a192ea23917817
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-09 15:37:23 +0200

    frontend: add theater add modal

---
+++ frontend/src/components/ui/Models/TheaterAdd.tsx
@@ -0,0 +1,31 @@
+import { GlobalStateContext } from "../../../context/GlobalState";
+import { useContext, useRef, useState } from 'react';
+import ReactDom from "react-dom";
+import { EditText } from 'react-edit-text';
+import 'react-edit-text/dist/index.css';
+
+
+const TheaterAdd = ({ setShowModal }) => {
+
+    const userInfo = useContext(GlobalStateContext)
+
+    const modalRef = useRef<HTMLInputElement>(null);
+
+    const handleAddTheater = async () => {
+
+    }
+
+    const test = `http://www.omdbapi.com/?apikey=8c589a6b&t=`
+
+    return ReactDom.createPortal(
+        <div className="container" ref={modalRef} onClick={(e) => e.target === modalRef.current && setShowModal(false)}>
+            <div className="modal">
+                <button onClick={() => setShowModal(false)}>X</button>
+                <input type='file' multiple></input>
+                <button onClick={handleAddTheater}>Finalize Theater</button>
+            </div>
+        </div>,
+        document.getElementById("theater-add") as HTMLElement
+    )
+}
+export default TheaterAdd;
