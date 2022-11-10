70f2bbb49437303cd0d2ad37231f2d87456c8da1
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-10 17:42:57 +0200

    backend: add stream video theater

--- backend/socket/src/services/functionalities/index.ts
+++ backend/socket/src/services/functionalities/index.ts
@@ -6,7 +6,6 @@ import { userCredentials, SubscribeMessage } from '../../utils';
 import validateSignature from '../middleware/validateUser';
 import axios from 'axios';
 import services from './service.class';
-import fs from 'fs';
 
 declare module 'socket.io' {
     interface Socket {
@@ -23,51 +22,6 @@ export default async (app: Express, io: Server<any, any, any, any>, channel: Cha
     let user: any = {};
     SubscribeMessage(channel, service)
 
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    });
-
-    app.get('/video/:id', (req, res) => {
-
-        const videoPath = `${__dirname}/assests/${req.params.id}.mp4`;
-        const videoStat = fs.statSync(videoPath);
-        const fileSize = videoStat.size;
-        const videoRange = req.headers.range;
-
-        if (videoRange) {
-            const parts = videoRange.replace(/bytes=/, "").split("-");
-            const start = parseInt(parts[0], 10);
-
-            const end = parts[1]
-                ? parseInt(parts[1], 10)
-                : fileSize - 1;
-
-            const chunksize = (end - start) + 1;
-            const file = fs.createReadStream(videoPath, { start, end });
-
-            const header = {
-                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                'Accept-Ranges': 'bytes',
-                'Content-Length': chunksize,
-                'Content-Type': 'video/mp4',
-            };
-
-            res.writeHead(206, header);
-            file.pipe(res);
-
-        } else {
-
-            const header = {
-                'Content-Length': fileSize,
-                'Content-Type': 'video/mp4',
-            };
-
-            res.writeHead(200, header);
-            fs.createReadStream(videoPath).pipe(res);
-
-        }
-
-    });
     const HomeIo = io.of('/main');
 
     HomeIo.on('connection', (socket) => {
