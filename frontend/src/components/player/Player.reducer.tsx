30fb2610c2050523645222b24da495b0102b3b16
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-15 11:57:30 +0200

    frontend: add video player reducer

---
+++ frontend/src/components/player/Player.reducer.tsx
@@ -0,0 +1,40 @@
+import { ReactPlayerProps } from 'react-player';
+import io, { Socket } from "socket.io-client";
+import { useContext } from 'react';
+import { GlobalStateContext } from '../../context/GlobalState';
+
+const INITIAL_STATE: ReactPlayerProps = {
+  playing: false,
+  controls: false,
+  volume: 0.8,
+  light: true,
+  progress: {
+    playedSeconds: 0,
+  },
+  duration: 0,
+};
+
+
+const reducer = (state: ReactPlayerProps, action: ReactPlayerProps) => {
+    switch (action.type) {
+      case 'PLAY':
+        return { ...state, playing: true };
+      case 'PAUSE':
+        return { ...state, playing: false };
+      case 'TOGGLE_PLAY':
+        return { ...state, playing: !state.playing };
+      case 'DURATION':
+        return { ...state, duration: action.payload };
+      case 'SEEK':
+        return { ...state, progress: { playedSeconds: action.payload } };
+      case 'VOLUME':
+        return { ...state, volume: action.payload };
+      case 'LIGHT':
+        return { ...state, light: action.payload };
+      default:
+        return state;
+    }
+
+};
+
+export { reducer, INITIAL_STATE };
