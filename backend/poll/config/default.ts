107987f7be0cf93b9acc7323ede06815620dbe6a
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-15 12:08:56 +0200

    backend: add poll config channel

--- backend/poll/config/default.ts
+++ backend/poll/config/default.ts
@@ -11,4 +11,7 @@ export default {
     PORT: process.env.PORT as unknown as number,
     DB_URL: process.env.MONGODB_URI,
     APP_SECRET: process.env.APP_SECRET,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+    THEATER_SERVICE: "THEATER_SERVICE",
 };
