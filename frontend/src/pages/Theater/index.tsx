ea219b28a94961c909aa392aec86ecba24ddcea2
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-16 20:41:42 +0200

    frontend: refactor theater page

--- frontend/src/pages/Theater/index.tsx
+++ frontend/src/pages/Theater/index.tsx
@@ -1,43 +1,22 @@
 import { useContext, useEffect, ReactElement, useRef, useState } from "react";
 import { GlobalStateContext } from "../../context/GlobalState";
 import io, { Socket } from "socket.io-client";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Movie from "../../components/screen/Movie";
 import SideBar from "../../components/screen/SideBar";
 
 
 const TheaterPage: () => ReactElement<any, any> = () => {
     const userInfo = useContext(GlobalStateContext);
+    const location = useLocation();
     const navigate = useNavigate();
-    let chatSocket: Socket | null = null;
-    let pollSocket: Socket | null = null;
-    let movieSocket: Socket | null = null;
-
-    if (userInfo.user.jwt !== '')
-        chatSocket = io('http://localhost:80/theater', {
-            auth: {
-                theater: '63593e5753ddddc1b35bc72d',
-                token: userInfo.user.jwt
-            }
-        })
-
-    if (userInfo.user.jwt !== '')
-        pollSocket = io('http://localhost:80/poll', {
-            auth: {
-                theater: '63593e5753ddddc1b35bc72d',
-                token: userInfo.user.jwt
-            }
-        })
-
-
-    if (userInfo.user.jwt !== '')
-        movieSocket = io('http://localhost:80/movie', {
-            auth: {
-                theater: '63593e5753ddddc1b35bc72d',
-                token: userInfo.user.jwt
-            }
-        })
-
+    const [url, setUrl] = useState(location.state.url)
+    const [movieSocket, setMovieSocket] = useState<Socket | null>(null);
+    const [chatSocket, setChatSocket] = useState<Socket | null>(null);
+    const [pollSocket, setPollSocket] = useState<Socket | null>(null);
+    // let chatSocket: Socket | null = null;
+    // let pollSocket: Socket | null = null;
+    // let movieSocket: Socket | null = null;
 
     const auth: any = async () => {
         await userInfo.auth();
@@ -47,13 +26,43 @@ const TheaterPage: () => ReactElement<any, any> = () => {
     };
 
     useEffect(() => {
-        auth();
-    }, [])
+        (async () => {
+            await auth();
+            if (location.state.theaterId === null)
+                navigate('/');
+            if (userInfo.user.jwt !== '')
+                setChatSocket(io('http://localhost:80/theater', {
+                    auth: {
+                        theater: location.state.theaterId,
+                        token: userInfo.user.jwt
+                    }
+                })
+                )
+            if (userInfo.user.jwt !== '')
+                setPollSocket(io('http://localhost:80/poll', {
+                    auth: {
+                        theater: location.state.theaterId,
+                        token: userInfo.user.jwt
+                    }
+                })
+                )
+
+            if (userInfo.user.jwt !== '') {
+                setMovieSocket(io('http://localhost:80/movie', {
+                    auth: {
+                        theater: location.state.theaterId,
+                        token: userInfo.user.jwt
+                    }
+                }))
+            }
+        })();
+    }, []);
+
 
     return (
         <>
-            <Movie movieSocket={movieSocket} />
-            <SideBar chatSocket={chatSocket} pollSocket={pollSocket} />
+            <Movie movieSocket={movieSocket} image={location.state.image} url={url} name={location.state.name} setUrl={setUrl} />
+            <SideBar chatSocket={chatSocket} movieSocket={movieSocket} playlist={location.state.playlist} setUrl={setUrl} />
         </>
     )
 }
