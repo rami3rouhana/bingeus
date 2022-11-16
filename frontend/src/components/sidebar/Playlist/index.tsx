68c299eec3601436acfe2cc0b8717a2e2ac84984
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-16 20:46:45 +0200

    frontend: add theater playlist

--- frontend/src/components/sidebar/Playlist/index.tsx
+++ frontend/src/components/sidebar/Playlist/index.tsx
@@ -1,14 +1,17 @@
 import { GlobalStateContext } from "../../../context/GlobalState";
 import { useContext, useState, useRef } from 'react';
 
-const Playlist = ({ socket, show }) => {
-    const userInfo = useContext(GlobalStateContext);
+const Playlist = ({ playlist, setUrl, movieSocket, show }) => {
 
     return (
         <>
             {
                 show === 'Playlist' ?
                     <>
+                        {
+                            playlist.map(movie => {
+                                return <div key={movie._id} onClick={(e) => { setUrl(movie.url); movieSocket.emit('handle change', movie.url) }}>{movie.name}</div>
+                            })}
                     </>
                     : false
             }
