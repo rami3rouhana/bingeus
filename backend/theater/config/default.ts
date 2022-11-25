9661f5816da677fb106b882b8b2a017379d5043e
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-26 01:08:21 +0200

    backend: refactor theater config

--- backend/theater/config/default.ts
+++ backend/theater/config/default.ts
@@ -1,17 +1,17 @@
 import dotEnv from "dotenv";
 
 if (process.env.npm_lifecycle_event !== "prod") {
-    const configFile = `./${process.env.npm_lifecycle_event}.env`;
+    const configFile = `../${process.env.npm_lifecycle_event}.env`;
     dotEnv.config({ path: configFile });
 } else {
-    dotEnv.config();
+    dotEnv.config(`../.env`);
 }
 
 export default {
-    PORT: process.env.PORT as unknown as number,
+    PORT: process.env.THEATER_PORT as unknown as number,
     DB_URL: process.env.MONGODB_URI,
     APP_SECRET: process.env.APP_SECRET,
-    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    EXCHANGE_NAME: "BINGE_US",
     MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
     SOCKET_SERVICE: "SOCKET_SERVICE",
     THEATER_SERVICE: "THEATER_SERVICE",
