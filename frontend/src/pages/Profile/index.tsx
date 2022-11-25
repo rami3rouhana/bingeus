2250280db02401929663e58ce3d9321d69d41b1f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-26 00:50:58 +0200

    frontend: refactor profile

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -4,6 +4,9 @@ import UserTheaters from "../../components/screen/UserTheaters";
 import { useContext, useEffect, ReactElement, } from "react";
 import { GlobalStateContext } from "../../context/GlobalState";
 import { useNavigate } from "react-router-dom";
+import './index.css';
+import LogoutButton from '../../components/assets/LogoutButton.svg';
+import BuLogo from '../../components/assets/BuLogo.png';
 
 const ProfilePage: () => ReactElement<any, any> = () => {
     const userInfo = useContext(GlobalStateContext);
@@ -19,11 +22,29 @@ const ProfilePage: () => ReactElement<any, any> = () => {
     }, [])
 
     return (
-        <>
-            <UserInfo />
-            <BlockList />
-            <UserTheaters />
-        </>
+        <div className="profile-page">
+            <div className="header">
+                <img src={BuLogo} onClick={() => navigate('/')}/>
+                <div className="user-display">{
+                    userInfo.user.loggedIn ?
+                        <><div className="user-profile-header" onClick={() => navigate('/profile')}><img src={`http://localhost/image/` + userInfo.user.image} /><span className="display-user-name">{userInfo.user.name}</span></div><img onClick={() => { userInfo.logout(); window.location.reload(); }} className="logout-button" src={LogoutButton} /></> :
+                        <><button className="user-edit-btn" onClick={() => navigate('login')}>Sign In</button></>
+
+                }</div>
+            </div>
+            <div className="page-content">
+                <div className="user-info-bar">
+                    <UserInfo />
+                    <BlockList />
+                </div>
+                <div>
+                    <h1>Your Theatres</h1>
+                    <div className="profile-theaters">
+                        <UserTheaters />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default ProfilePage;
