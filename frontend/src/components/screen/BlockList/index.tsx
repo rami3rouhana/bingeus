7c7843940587216808505df5fbebe09b86ad45b8
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-26 00:58:21 +0200

    frontend: refactor blocklist

--- frontend/src/components/screen/BlockList/index.tsx
+++ frontend/src/components/screen/BlockList/index.tsx
@@ -1,21 +1,24 @@
 import { GlobalStateContext } from "../../../context/GlobalState";
 import { useContext } from 'react';
+import UnblockButton from '../../assets/UnblockButton.svg';
+import './index.css';
 
 const BlockList = () => {
     const userInfo = useContext(GlobalStateContext);
     return (
-        <>
-            <ul>
+        <div className="blocked-list-profile">
+            <span className='block-title'>Blocked List</span>
+            <ul className="blocked-list">
                 {userInfo.user.blockedList.map((user: any) => {
                     return (
                         <li key={user._id}>
-                            <img src={user.image} />
+                            <img src={'http://localhost/image/' + user.image} />
                             <span>{user.name}</span>
-                            <button onClick={async () => await userInfo.unblock(user.userId)}>Unblock</button>
+                            <button onClick={async () => await userInfo.unblock(user.userId)}><img src={UnblockButton} /></button>
                         </li>)
                 })}
             </ul>
-        </>
+        </div>
     )
 }
 export default BlockList;
