abd92e8628c2ca99a51e4c6140f5ed32bb566ec4
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-11-26 00:52:08 +0200

    frontend: refactor login

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalStateContext } from "../../context/GlobalState";
+import './index.css';
+import GirlProfile from '../../components/assets/GirlProfile.png';
 
 const LoginPage = () => {
     const userInfo = useContext(GlobalStateContext);
@@ -23,11 +25,36 @@ const LoginPage = () => {
     }
 
     return (
-        <>
-            <input type='text' placeholder='Email' onChange={(e) => { setEmail(e.currentTarget.value) }} />
-            <input type='password' placeholder='Password' onChange={(e) => { setPassword(e.currentTarget.value) }} />
-            <button onClick={handleSubmit}>Login</button>
-        </>
+        <div className="login-signin">
+            <div className="left-pannel">
+                <div className="login-message">
+                    <span>
+                        Welcome Back
+                    </span>
+                </div>
+                <div className="login-div-container">
+                    <span className="login-title">Login</span>
+                    <input className="input" type='text' placeholder='Email' onChange={(e) => { setEmail(e.currentTarget.value) }} />
+                    <input className="input" type='password' placeholder='Password' onChange={(e) => { setPassword(e.currentTarget.value) }} />
+                    <button onClick={handleSubmit}>Login</button>
+                    <div className="signup-link">
+                        <span className="signup-link-left">Don’t have an account?</span>
+                        <span className="signup-link-right" onClick={() => navigate('/signup')}> Signup</span>
+                    </div>
+                </div>
+            </div>
+            <div className="right-pannel" id="left-pannel">
+                <span className="right-pannel-hero-span">
+                    “You made it so simple. My friends and I can now watch all the movies and series together...”
+                </span>
+                <div className="user-quote">
+                    <img src={GirlProfile} />
+                    <span>
+                        Leslie Alexander
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 export default LoginPage;
